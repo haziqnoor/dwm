31d3e513072498fe2e430555b56f40585bb1b61c
Haziq Noor <haziq@protonmail.com>
2017-11-27 14:56:18 +0800

    Syncing colors

--- config.h
+++ config.h
@@ -7,13 +7,13 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Titillium:size=11" };
 static const char dmenufont[]       = "Titillium:size=11";
-static const char col_darkgray[]    = "#000";
+static const char col_black[]    = "#000";
 static const char col_lightgray[]   = "#b2b2b2";
 static const char col_red[]       = "#af0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_lightgray, col_darkgray, col_darkgray },
-	[SchemeSel] =  { col_red, col_darkgray,  col_darkgray  },
+	[SchemeNorm] = { col_lightgray, col_black, col_black },
+	[SchemeSel] =  { col_red, col_black,  col_black  },
 };
 
 /* tagging */
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_darkgray, "-nf", col_lightgray, "-sb", col_darkgray, "-sf", col_red, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_lightgray, "-sb", col_black, "-sf", col_red, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *statcmd[]  = { "statusbar", NULL };
 
